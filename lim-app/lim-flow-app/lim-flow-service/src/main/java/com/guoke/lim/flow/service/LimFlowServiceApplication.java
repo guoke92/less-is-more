32a6014a75669f2d5fc70e2e12ed19d112b8fc12
guoke92 <1542071126@qq.com>
2022-03-14 16:35:38 +0800

    build(main): init project

---
+++ lim-app/lim-flow-app/lim-flow-service/src/main/java/com/guoke/lim/flow/service/LimFlowServiceApplication.java
@@ -0,0 +1,14 @@
+package com.guoke.lim.flow.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guoke92
+ */
+@SpringBootApplication
+public class LimFlowServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimFlowServiceApplication.class, args);
+    }
+}
