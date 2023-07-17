57bb696d14f1d3f0d1cb34fe1c2b576f8dbd5166
fanjunwei <fanjunwei@linklogis.com>
2023-07-17 15:36:08 +0800

    commit sth

--- lim-framework/lim-common/lim-common-snowflake/src/test/java/com/guoke/lim/common/snowflake/service/SequenceTest.java
+++ lim-framework/lim-common/lim-common-snowflake/src/test/java/com/guoke/lim/common/snowflake/service/SequenceTest.java
@@ -12,23 +12,52 @@ import java.util.stream.Stream;
  */
 public class SequenceTest {
 
-    public static void main(String[] args) {
-        Sequence sequence = new Sequence();
+    public class Sequence {
 
-        Set<Long> idSet = new CopyOnWriteArraySet<>();
+    private static final Logger logger = LoggerFactory.getLogger(Sequence.class);
 
-        List<Integer> collect = Stream.iterate(1, x -> ++x).limit(100000).collect(Collectors.toList());
+    /**
+     * 起始的时间戳
+     */
+    private final static long START_STMP = 1480166465631L;
 
-        //并发获取雪花id
-        collect.stream().parallel().forEach(item -> {
-            long snowId = sequence.getSnowId();
-            idSet.add(snowId);
-            System.out.print("snowId = " + snowId);
-            System.out.println("========idSet = " + idSet.size());
-        });
+    /**
+     * 每一部分占用的位数
+     */
+    private final static long SEQUENCE_BIT = 12; //序列号占用的位数
+    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
+    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
 
-        System.out.println("collect = " + collect.size());
-        System.out.println("idSet = " + idSet.size());
+    /**
+     * 每一部分的最大值
+     */
+    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
+    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
 
+    /**
+     * 每一部分向左的位移
+     */
+    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+
+    private long datacenterId;  //数据中心
+    private long machineId;     //机器标识
+    private long sequence = 0L; //序列号
+    private long lastStmp = -1L;//上一次时间戳
+
+    public Sequence(long datacenterId, long machineId) {
+        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
+            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.datacenterId = datacenterId;
+        this.machineId = machineId;
     }
+
+    /**
+
 }
